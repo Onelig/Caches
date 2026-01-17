4a853ba8823a267ad8e83acc7152d670c30c6c6c
Onelig <onelig.dark@gmail.com>
2026-01-17 12:14:26 +0000

    Create LRU template class

--- include/caches/LRU/LRU.hpp
+++ include/caches/LRU/LRU.hpp
@@ -1,1 +1,49 @@
 #pragma once
+#include <unordered_map>
+
+namespace cache
+{
+	template<typename Key, typename Value>
+	class LRU
+	{
+	private:
+		class Node
+		{
+			std::pair<Key, Value> val;
+			Node* next;
+			Node* prev;
+			Node(Key key, Value value)
+			{
+				val.first = key;
+				val.second = value;
+
+				next = nullptr;
+				prev = nullptr;
+			}
+		};
+
+	public:
+		LRU(unsigned capacity);
+		~LRU();
+
+	private:
+		std::unordered_map<Key, Node*> cacheUMap;
+		Node* begin = new Node(-1, -1);
+		Node* end   = new Node(-1, -1);
+		unsigned capacity;
+	};
+
+
+	template<typename Key, typename Value>
+	LRU<Key, Value>::LRU(unsigned capacity)
+		: capacity(capacity)
+	{ }
+
+	template<typename Key, typename Value>
+	LRU<Key, Value>::~LRU()
+	{
+		delete begin;
+		// + other fields it future
+		delete end;
+	}
+}
